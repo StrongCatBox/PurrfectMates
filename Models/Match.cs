ff861f238afbe0985aa53931c0830c6459f67976
unknown <saljei055@gmail.com>
2025-09-02 12:21:18 +0200

    update auth working

--- Models/Match.cs
+++ Models/Match.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PurrfectMates.Models
 {
+
+    [Table("Matching")]
     public class Match
     {
         [Key] public int IdMatch { get; set; }
